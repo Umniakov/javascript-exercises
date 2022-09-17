eeb21389bce12cf403da32f40895540b94b971fe
Stanislav <Arcanalord007@gmail.com>
2022-09-17 22:23:41 +0300

    Make 3 of them

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function(text, times) {
+    if (times < 0) return 'ERROR';
+    let outp ='';
+    for (let i = 0; i < times; i++) {
+        outp += text;
+    }
+    return outp;
 };
 
+// console.log(repeatString( , 3));
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+// const reverseString = function(string) {
+//     let arr1;
+//     arr1 = string.split('');
+//     let newString = '';
+//     for (let i = arr1.length - 1; i >= 0 ; i--) {
+//         newString += arr1[i];
+//     }
+//     return newString;
+//     console.log(arr1);
+//     console.log(newString);
+// };
 
-};
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
+}
 
+console.log(reverseString('blabla'));
 // Do not edit below this line
 module.exports = reverseString;
