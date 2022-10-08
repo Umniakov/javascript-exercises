753b584e036ecf6c06eaaeeff0882f37d870c132
Stanislav <Arcanalord007@gmail.com>
2022-10-08 05:17:58 +0300

    Make 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  if (isNotEmpty(args)) {
+	let outcome = args.reduce((total, current) => total + current);
+  return outcome;
+  } else {
+    return 0;
+  }
 };
 
-const multiply = function() {
+function isNotEmpty(args) {
+  for (let key in args) {
+    return true;
+  }
+  return false;
+}
 
+const multiply = function(arr) {
+  let outcome = arr.reduce((total, current) => total * current);
+  return outcome;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (Math.pow(a, b));
 };
 
-const factorial = function() {
-	
-};
+const factorial = (n => n <= 1 ? 1 : n * factorial(n -1));
 
 // Do not edit below this line
 module.exports = {
