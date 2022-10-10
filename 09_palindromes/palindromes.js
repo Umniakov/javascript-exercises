66d027cec6d1c794dd54cb8c814b78c3922781f0
Stanislav <Arcanalord007@gmail.com>
2022-10-10 05:17:55 +0300

    Make palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function(test) {
+  let replace = test.toLowerCase().replaceAll(/[^a-z]/g, '');;
+  const backwards = replace
+    .split('')
+    .reverse()
+    .join('')
+    return backwards === replace;
+}
 // Do not edit below this line
 module.exports = palindromes;
