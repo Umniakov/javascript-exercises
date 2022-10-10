20f44a6f11b409c90546d2a13f3eb2c788823aca
Stanislav <Arcanalord007@gmail.com>
2022-10-10 07:58:19 +0300

    Make findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+// const findTheOldest = function(people) {
+//     let currentYear = (new Date()).getFullYear();
+//     people.map(e => {
+//       if (e.yearOfDeath === undefined) {
+//         e.years = currentYear - e.yearOfBirth;
+//       } else {
+//         e.years = e.yearOfDeath - e.yearOfBirth;
+//       }
+//     })
+//     people.sort((a, b) => a.years < b.years ? 1 : -1); 
+//     return people[0];
+//   };
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+      const oldestP = age(oldest.yearOfBirth, oldest.yearOfDeath)
+      const currentP = age(current.yearOfBirth, current.yearOfDeath)
+      return oldestP > currentP ? oldest : current;
+    })
 };
+  
+function age(birth, death) {
+    if(!death) { 
+        death = new Date().getFullYear();
+    }
+        return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
