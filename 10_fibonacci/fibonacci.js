5dd2849470b8dc58e61263be739f8521b7314783
Stanislav <Arcanalord007@gmail.com>
2022-10-10 06:03:10 +0300

    Make fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(number) {
+    let num = Number(number)
+    if (num === 1 || num === 2) return 1;
+    if (num > 2) {
+      let x = 1;
+      let y = 1;
+      let temp = x;
+      for (let i = 2; i < num; i++) {
+        temp = x;
+        x = x + y;
+        y = temp;
+      }
+      return x;
+    } else {
+        return 'OOPS';
+    }
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
